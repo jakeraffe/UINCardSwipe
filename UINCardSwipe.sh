34286ec475d42e0c6f13ea6c60fcf057ddc87217
jakeraffe <32085971+jakeraffe@users.noreply.github.com>
2019-02-23 18:08:46 -0600

    Comments added.
    Added comments to code.

--- UINCardSwipe.sh
+++ UINCardSwipe.sh
@@ -13,14 +13,30 @@ do
         fi
         clear
 
+        #-d declares the next character will be a delimiter.
+        #   Here, the data starts with a "B" followed by the card number.
+        #   The card number ends with a "^" so we put a second delimiter called f1.
         num="$(echo "$data"|cut -d\B -f2|cut -d\^ -f1)"
+
+        #here, the name is split in two parts spearated by a "/".
+        #   the full name ends with a "^" so that is our delimiter
         name="$(echo "$data"|cut -d\^ -f2)"
+        # helps us select which part of the name e.g. "asas/ffff" we choose
         firstname="$(echo "$name"|cut -d/ -f1)"
         secondname="$(echo "$name"|cut -d\/ -f2)"
+
+        #TODO
+        # Finds the UIN which is on the same line as the second instance
+        #   of the experation. Might be tricky..
         UIN="$(echo "$UIN"|cut -d )"
+
+        #TODO
+        # Dont know if this information is necessary, but it will remain
+        #    for now and is functional.
         exdate="$(echo "$data"|cut -d\^ -f3)"
         expdate="${expdate:2:2}/${exdate:0:2}"
 
+        # Printing out the variables.
         echo "User: $firstname $secondname"
         echo "Card number: $num"
         echo "UIN: $UIN"
